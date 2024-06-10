aa21568bc99456f7a8bfee2e4eb815f373964b02
TajCh <TajCh@MSI.mshome.net>
2024-06-10 09:15:40 +0530

    updated producer sending data

--- KafkaAssignment/src/main/java/com/taj/kafka/producer/kafkaproducer.java
+++ KafkaAssignment/src/main/java/com/taj/kafka/producer/kafkaproducer.java
@@ -14,8 +14,11 @@ public class kafkaproducer {
 		
 		
 		try (KafkaProducer<Long, String> producer = new KafkaProducer<>(props)) {
-			ProducerRecord<Long, String> record = new ProducerRecord<>("TruckTopic",01L, "22.5726N,88.3639E");
-			producer.send(record, new TrackCallback());
+			//ProducerRecord<Long, String> record = new ProducerRecord<>("TruckTopic",01L, "22.5726N,88.3639E");
+			//producer.send(record, new TrackCallback());
+			producer.send(new ProducerRecord<Long, String>("TruckTopic",01L,"41.72556N,49.94694W"));
+            producer.send(new ProducerRecord<Long, String>("TruckTopic", 02L,"37.4205N,24.1702E"));
+            producer.send(new ProducerRecord<Long, String>("TruckTopic",03L, "49.25N,10.25W"));
 		
 		}catch(Exception e) {
 			e.printStackTrace();
